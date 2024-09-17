6e37c3f313065a0cc6acffc94a4156583c15c5d2
HarrisBrandon-FS1 <162524452+HarrisBrandon-FS1@users.noreply.github.com>
2024-09-16 20:50:16 -0500

    added controllers

--- app/routes/authorRoutes.js
+++ app/routes/authorRoutes.js
@@ -1,55 +1,21 @@
 const router = require("express").Router();
+const { 
+    createAuthor,
+    getAuthorById,
+    getAllAuthors,
+    updateAuthor,
+    deleteAuthor} = require("../controller/authorController")
 
-router.get("/", (req, res) => {
-    res
-    .status(200)
-    .json({
-        success: true,
-        message: `${req.method} - request to Author endpoint`,
-    });
-});
+router.get("/", getAllAuthors);
 
-router.get("/:id", (req, res) => {
-    const { id } = req.params;
-    res
-    .status(200)
-    .json({
-        id,
-        success: true,
-        message: `${req.method} - request to Author endpoint`,
-    });
-});
+router.get("/:id", getAuthorById);
 
-router.post("/", (req, res) => {
-    res
-    .status(200)
-    .json({
-        success: true,
-        message: `${req.method} - request to Author endpoint`,
-    });
-});
 
-router.put("/:id", (req, res) => {
-    const { id } = req.params;
-    res
-    .status(200)
-    .json({
-        id,
-        success: true,
-        message: `${req.method} - request to Author endpoint`,
-    });
-});
+router.post("/", createAuthor)
 
-router.delete("/:id", (req, res) => {
-    const { id } = req.params;
-    res
-    .status(200)
-    .json({
-        id,
-        success: true,
-        message: `${req.method} - request to Author endpoint`,
-    });
-});
+router.put("/:id", updateAuthor);
+
+router.delete("/:id", deleteAuthor);
 
 
 module.exports = router;
